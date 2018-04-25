c5ef049b399986d8965eb39cddf2c55e18ea91d9
Alexey Khrusch <alexeykhr@outlook.com>
2018-04-25 20:29:55 +0300

    Fixed docs links again

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/frontend', // TODO Temporary (for github)
 
     /**
      * Source Maps
