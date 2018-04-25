8530e88ba6f9877f997a36b99c1f75dd1a761383
Alexey Khrusch <alexeykhr@outlook.com>
2018-04-25 20:33:27 +0300

    Fixed slash

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/frontend', // TODO Temporary (for github)
+    assetsPublicPath: '/frontend/', // TODO Temporary (for github)
 
     /**
      * Source Maps
