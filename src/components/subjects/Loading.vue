93db94d6a04b0b9656a1a3dd3da81e662f623008
Alexey Khrusch <alexeykhr@outlook.com>
2018-05-03 11:43:57 +0300

    Small corrected loading + support dark theme

--- src/components/subjects/Loading.vue
+++ src/components/subjects/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="'loading ' + theme">
-    <div class="item" v-for="n in 10" :style="{ width: getRndInteger(60, 140) + 'px' }" :key="n"></div>
+    <div class="item" v-for="n in 10" :style="{ width: getRndInteger(90, 180) + 'px' }" :key="n"></div>
   </div>
 </template>
 
@@ -23,38 +23,44 @@ export default {
 .loading {
   display: flex;
   flex-wrap: wrap;
-  > .item {
-    position: relative;
-    margin: 10px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    cursor: context-menu;
-    letter-spacing: 2px;
-    height: 39px;
-    // FIXME To global*
-    &:after {
-      content: '';
-      box-sizing: border-box;
-      animation-duration: 1s;
-      animation-fill-mode: forwards;
-      animation-iteration-count: infinite;
-      animation-name: loading;
-      animation-timing-function: linear;
-      background: #eeeeee;
-      background: linear-gradient(to right, #eeeeee 8%,#dddddd 18%, #eeeeee 33%);
-      background-size: 800px 104px;
-      position: absolute;
-      height: 100%;
-      width: 100%;
-      top: 0;
-      left: 0;
-    }
+}
+
+.item {
+  position: relative;
+  margin: 10px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  cursor: context-menu;
+  letter-spacing: 2px;
+  height: 39px;
+  &:after {
+    content: '';
+    box-sizing: border-box;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: loading;
+    animation-timing-function: linear;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+}
+
+.light {
+  .item:after {
+    background: #f3f3f3;
+    background: linear-gradient(to right, #f3f3f3 8%, #fbfbfb 18%, #f9f9f9 33%);
+    background-size: 800px 100px;
   }
 }
 
 .dark {
-  .item {
-    background-color: #444;
-    color: rgba(255, 255, 255, .87);
+  .item:after {
+    background: #4c4c4c;
+    background: linear-gradient(to right, #4c4c4c 8%, #424242 18%, #444 33%);
+    background-size: 800px 100px;
   }
 }
 </style>
