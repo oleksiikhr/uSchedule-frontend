e37aba08f31e9371be26fb6ed8db9b55acd640d2
Alexey Khrusch <alexeykhr@outlook.com>
2018-08-29 22:37:42 +0300

    package.json - add dynamic-import-node, fix babel

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = {
   presets: [
     ["@babel/preset-env", {
       "modules": false
-    }],
-    ["@babel/preset-stage-2", {
-      decoratorsLegacy: true
     }]
   ],
-  plugins: ["transform-vue-jsx", "@babel/plugin-transform-runtime"]
+  plugins: ["transform-vue-jsx", "@babel/plugin-transform-runtime", "dynamic-import-node"]
 }
