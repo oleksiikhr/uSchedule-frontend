6a24b7037443f482a425b8279332eecaffb8c17e
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 23:26:06 +0300

    Update header, -webkit-scrollbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,41 @@
 <template>
   <header id="header">
     <div class="header__clock">
-      <!--TODO link to time-->
-      <el-button icon="el-icon-time" circle></el-button>
+      <router-link :to="{ name: 'timetable' }">
+        <i class="material-icons">access_time</i>
+      </router-link>
     </div>
     <div class="header__logo">
-      LOGO
+      <router-link :to="{ name: 'home' }">
+        <strong style="font-size: 1.2rem; border-bottom: 1px solid;">uSchedule</strong>
+      </router-link>
     </div>
     <div class="header__find">
-      <!--TODO Search (schedule, teacher)-->
-      <el-button icon="el-icon-search" circle></el-button>
+      <!--TODO Search, full-height (schedule, teacher)-->
+      <a @click="openSearch">
+        <i class="material-icons">search</i>
+      </a>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    openSearch () {
+      console.log('Open search')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+a {
+  display: block;
+  line-height: 0;
+  padding: 0 20px;
+}
+
 #header {
   display: flex;
   align-items: center;
