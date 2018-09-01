be80ccca6dda7937e97c229bae8f73d029af89e6
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 21:05:50 +0300

    webpack - styles are in a separate file

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,7 +23,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['./src/main.js', './src/assets/styles/index.scss'],
   },
   output: {
     path: config.build.assetsRoot,
