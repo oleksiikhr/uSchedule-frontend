3fb47126c445c8c177bed7ff66631deb5f510753
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-01 23:14:31 +0300

    Moment.js remove from main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import ElementUiLangRu from 'element-ui/lib/locale/lang/ru-RU'
 import ElementUi from 'element-ui'
 import router from './router'
 import store from './store'
-import moment from 'moment'
 import App from './App'
 import Vue from 'vue'
 
-// Connect files, change the language.
+// Vue use components.
 Vue.use(ElementUi, ElementUiLangRu)
-moment.locale('ru')
 
 // Prevent the production tip on Vue startup.
 Vue.config.productionTip = false
