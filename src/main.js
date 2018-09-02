8fc961d24c619ef92721882669dd167e7914d475
Alexey Khrusch <alexeykhr@outlook.com>
2018-09-02 09:52:25 +0300

    package.json - add normalize.css, remove element-ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-import ElementUiLangRu from 'element-ui/lib/locale/lang/ru-RU'
-import ElementUi from 'element-ui'
 import router from './router'
 import store from './store'
 import App from './App'
 import Vue from 'vue'
-
-// Vue use components.
-Vue.use(ElementUi, ElementUiLangRu)
 
 // Prevent the production tip on Vue startup.
 Vue.config.productionTip = false
